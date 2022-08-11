f16b525be65e219f9c1bd662614d4d98e6b87e01
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2022-08-11 19:03:41 +0530

    ssssss

--- seleniumQSPIDERS/src/seleniumQSPIDERS/FB_Logout.java
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/FB_Logout.java
@@ -1,8 +1,8 @@
 package seleniumQSPIDERS;
 
 public class FB_Logout {
-public static void main(String[] args) {
-	System.out.println("AAA");
-	System.out.println("AAA");
-}
+	public static void main(String[] args) {
+		System.out.println("AAA");
+		System.out.println("AAA");
+	}
 }
