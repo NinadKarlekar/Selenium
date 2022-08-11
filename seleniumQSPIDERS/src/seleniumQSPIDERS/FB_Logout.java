04fc1d0deccc5a05847ae41dd9b64a778a4ea226
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2022-08-11 20:08:13 +0530

    dd

--- seleniumQSPIDERS/src/seleniumQSPIDERS/FB_Logout.java
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/FB_Logout.java
@@ -2,8 +2,6 @@ package seleniumQSPIDERS;
 
 public class FB_Logout {
 	public static void main(String[] args) {
-		System.out.println("AAA");
-		System.out.println("AAA");
-		System.out.println("AAA");
+		
 	}
 }
---
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/XpathTraversing.java
@@ -0,0 +1,25 @@
+package seleniumQSPIDERS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathTraversing {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.get(
+				"file:///F:/QSPIDERS/Selenium%20GIT/Selenium/seleniumQSPIDERS/src/seleniumQSPIDERS/xpathTraversing.html");
+
+		driver.findElement(By.xpath("//a[.='JAVA']/../../td[2]/a")).click();
+		driver.navigate().back();
+		driver.findElement(By.xpath("//a[.='SQL']/../../td[2]/a")).click();
+		driver.navigate().back();
+		driver.close();
+	}
+}
