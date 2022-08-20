a5d66fc54e57420b0e240cc07923faf2ef06abd3
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2022-08-20 23:03:29 +0530

    Create Implicit_Wait.java

---
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/Synchronization/Implicit_Wait.java
@@ -0,0 +1,26 @@
+package seleniumQSPIDERS.Synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Implicit_Wait {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");
+		driver.findElement(By.xpath("//a[.='Login ']")).click();
+		driver.findElement(By.id("logoutLink")).click();
+		driver.close();
+		
+	}
+}
