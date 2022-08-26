3d3708ef137a587c96acb16a9ef88648f26aeff1
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2022-08-26 23:33:19 +0530

    child browser

---
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/HiddendivPopup_CareInsurance_Automate.java
@@ -0,0 +1,40 @@
+package seleniumQSPIDERS;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class HiddendivPopup_CareInsurance_Automate {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://www.careinsurance.com/rhicl/proposalcp/renew/index-care");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='Policy Number']")).sendKeys("57124765");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@placeholder='DOB']")).click();
+		WebElement monthTB = driver.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+		Select s1 = new Select(monthTB);
+		s1.selectByVisibleText("Mar");
+		Thread.sleep(2000);
+		WebElement yearTB = driver.findElement(By.xpath("//select[@class='ui-datepicker-year']"));
+		Select s2 = new Select(yearTB);
+		s2.selectByVisibleText("2002");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[.='15']")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@placeholder='Contact Number']")).sendKeys("9988776655");
+		driver.findElement(By.xpath("//button[.='Lets Renew']")).click();
+		
+		Thread.sleep(5000);
+		driver.close();
+	}
+}
---
+++ seleniumQSPIDERS/src/seleniumQSPIDERS/Popups/Child_Browser_Popup.java
@@ -0,0 +1,39 @@
+package seleniumQSPIDERS.Popups;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Child_Browser_Popup {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/browser-windows");
+		Thread.sleep(2000);
+		String ParentWindowHandle = driver.getWindowHandle();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@id='windowButton']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@id='windowButton']")).click();
+		Set<String> allwindowhandle = driver.getWindowHandles();
+		for(String v:allwindowhandle)
+		{
+			if(v.equals(ParentWindowHandle)) {
+				
+			}
+			else {
+				driver.switchTo().window(v).close();
+				Thread.sleep(2000);
+			}
+		}
+		driver.close();
+	}
+}
