ff391794c6ef59181550d0153d4a2559d783fc19
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2022-09-30 23:56:33 +0530

    Update POM_DDS_actiTime_Login_driver.java

--- Frameworks/src/DataDrivenFramework/POM_DDS_actiTime_Login_driver.java
+++ Frameworks/src/DataDrivenFramework/POM_DDS_actiTime_Login_driver.java
@@ -12,31 +12,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class POM_DDS_actiTime_Login_driver {
-	static {
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	}
+    static {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
 
-	public static void main(String[] args)
-			throws EncryptedDocumentException, FileNotFoundException, IOException, InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		driver.get("https://demo.actitime.com/login.do");
+    public static void main(String[] args)
+            throws EncryptedDocumentException, FileNotFoundException, IOException, InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://demo.actitime.com/login.do");
 
-		POM_DDS_actiTime_Login_Encapsulated l1 = new POM_DDS_actiTime_Login_Encapsulated(driver);
+        POM_DDS_actiTime_Login_Encapsulated l1 = new POM_DDS_actiTime_Login_Encapsulated(driver);
 
-		String path = "./data/Book1.xlsx";
-		Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+        String path = "./data/Book1.xlsx";
+        Workbook wb = WorkbookFactory.create(new FileInputStream(path));
 
-		for (int i = 0; i < 6; i++) {
-			String un = wb.getSheet("input").getRow(i).getCell(0).toString();
-			String pw = wb.getSheet("input").getRow(i).getCell(1).toString();
+        for (int i = 0; i < 6; i++) {
+            String un = wb.getSheet("input").getRow(i).getCell(0).toString();
+            String pw = wb.getSheet("input").getRow(i).getCell(1).toString();
 
-			l1.setUsername(un);
-			l1.setPassword(pw);
-			l1.clickLogin();
-		}
-		Thread.sleep(5000);
-		driver.close();
-	}
+            l1.setUsername(un);
+            l1.setPassword(pw);
+            l1.clickLogin();
+        }
+        Thread.sleep(5000);
+        driver.close();
+    }
 }
